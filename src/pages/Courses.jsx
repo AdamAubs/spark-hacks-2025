9d4a6841a609db35974b528499e1e75ce6b02a70
oaduy2 <oaduy2@uic.edu>
2025-02-07 23:48:12 -0600

    courses v1 - implemented course navbar structure

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,15 +1,15 @@
 import './Courses.css'
+import CourseNav from '../components/CourseNav'
+
 
 export default function Courses() {
   return (
     <>
-      <ul id='link-history'>
-
-      </ul>
+      <CourseNav />
       
       <div id='course-container'>
         <div className='course-item'>
-
+          
         </div>
       </div>
     </>
