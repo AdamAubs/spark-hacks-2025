fb9b5a6eae2f94c3eecbc5515eb2b093da7338e1
Dominic Irla <irladominic@gmail.com>
2025-02-08 15:54:34 -0600

    Courses data + Grades page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Courses from "./pages/Courses";
-import Grades from "./pages/Grades";
+import GradesGrid from "./pages/GradesGrid";
 import MyCalendar from "./pages/MyCalendar";
 import Contacts from "./pages/Contacts";
+import Grades from "./pages/Grades";
 
 export default function App() {
   return (
@@ -13,7 +14,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/courses/:crn" element={<Courses />} />
         <Route path="/courses/:crn/contacts" element={<Contacts />} />
-        <Route path="/grades" element={<Grades />} />
+        <Route path="/courses/:crn/grades" element={<Grades />} />
+        <Route path="/grades" element={<GradesGrid />} />
         <Route path="/calendar" element={<MyCalendar />} />
       </Routes>
     </MainLayout>
