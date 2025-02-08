30702c8b998d2965ccc9508f8811ff84de8a3c09
Dominic Irla <irladominic@gmail.com>
2025-02-08 17:47:52 -0600

    Course Cards Size increase

--- src/components/CoursesGrid.jsx
+++ src/components/CoursesGrid.jsx
@@ -3,7 +3,7 @@ import CourseCard from "./CourseCard";
 
 const CoursesGrid = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       {courses.map((course, index) => (
         <CourseCard key={index} course={course} />
       ))}
